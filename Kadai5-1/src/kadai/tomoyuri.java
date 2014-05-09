2f060d3f6f8bf7f6db80b3effa9b2b74a44af1d8
g12940ot <g12940ot+git@s30415.tsuda.ac.jp>
2014-05-09 12:13:36 +0900

    1〜9 までできたので、10以降お願いします。

--- Kadai5-1/src/kadai/tomoyuri.java
+++ Kadai5-1/src/kadai/tomoyuri.java
@@ -1,10 +1,31 @@
 package kadai;
 
+import java.util.Scanner;
+
 public class tomoyuri {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner (System.in);
+		int input = sc.nextInt();
+		System.out.println(translateEng(input));
 	}
-
+		static String translateEng(int n) {
+			if(n < 10) return countEng1(n);
+			return n+"";
+			
+		}
+	
+	static String countEng1(int n){
+		if(n == 1) return "one";
+		if(n == 2) return "two";
+		if(n == 3) return "three";
+		if(n == 4) return "four";
+		if(n == 5) return "five";
+		if(n == 6) return "six";
+		if(n == 7) return "seven";
+		if(n == 8) return "eight";
+		if(n == 9) return "nine";
+		return "";
+	}
 }
