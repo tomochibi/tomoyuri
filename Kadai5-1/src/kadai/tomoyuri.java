723e4e8e3e527897d241aab0e2846dc6e5ad0786
g12940ot <g12940ot+git@s30536.tsuda.ac.jp>
2014-05-12 18:15:48 +0900

    ９９までできるようになりました

--- Kadai5-1/src/kadai/tomoyuri.java
+++ Kadai5-1/src/kadai/tomoyuri.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 public class tomoyuri {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner (System.in);
 		int input = sc.nextInt();
 		System.out.println(translateEng(input));
 	}
+	
 		static String translateEng(int n) {
-			if(n < 20) return countEng1(n);
+			int x=n/10;//10‚ÌˆÊ
+			int y=n-(x*10);//1‚ÌˆÊ
+			if(n < 10) return countEng1(n);
+			if(n>=11 && 19>=n) return countEng3(n);
 			if(n%10 == 0) return countEng2(n);
+			else if(n>20 && n<100) return countEng2(n)+" "+countEng1(y);
 			return n+"";
 			
 		}
 	
 	static String countEng1(int n){
-		String[] a = new  String[19];
+		String[] a = new  String[9];
 		
 		a[0] = "one";
 		a[1] = "two";
@@ -29,19 +33,9 @@ public class tomoyuri {
 		a[6] ="seven";
 		a[7] = "eight";
 		a[8] = "nine";
-		a[9] = "ten";
-		a[10] = "eleven";
-		a[11] = "twelve";
-		a[12] = "thirteen";
-		a[13] = "fourteen";
-		a[14] = "fifteen";
-		a[15] = "sixteen";
-		a[16] = "seventeen";
-		a[17] = "eighteen";
-		a[18] = "nineteen";
 		
 		
-		if(n>=1 && n<=19) {
+		if(n>=1 && n<=9) {
 			return a[n-1];
 		}
 		
@@ -49,26 +43,40 @@ public class tomoyuri {
 	}
 	
 	static String countEng2(int n){
-		int x,y;
-		String[] b = new String[8];
 		
+		String[] b = new String[9];
 		
-		b[0] = "twenty";
-		b[1] = "thirty";
-		b[2] = "forty";
-		b[3] = "fifty";
-		b[4] = "sixty";
-		b[5] = "seventy";
-		b[6] = "eighty";
-		b[7] = "ninety";
+		b[0] = "ten";
+		b[1] = "twenty";
+		b[2] = "thirty";
+		b[3] = "forty";
+		b[4] = "fifty";
+		b[5] = "sixty";
+		b[6] = "seventy";
+		b[7] = "eighty";
+		b[8] = "ninety";
 		
-		if(n%10 == 0) {
-			return b[n/10-1];
+	    return b[n/10-1];
+		
 			
 		}
-		return "";
+		
+	
+	static String countEng3(int n){
+		String[] c = new  String[9];
+		
+		
+		c[0] = "eleven";
+		c[1] = "twelve";
+		c[2] = "thirteen";
+		c[3] = "fourteen";
+		c[4] = "fifteen";
+		c[5] = "sixteen";
+		c[6] = "seventeen";
+		c[7] = "eighteen";
+		c[8] = "nineteen";	
+	
+	return "";
 		
 	}
-	
-	//static String count
 }
