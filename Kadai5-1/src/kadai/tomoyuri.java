d3b981d33e4f337e46bab71dc874a91e18c93bdb
g12940ot <g12940ot@s20527.tsuda.ac.jp>
2014-05-14 16:14:27 +0900

    0から119まで出るようになりました。

--- Kadai5-1/src/kadai/tomoyuri.java
+++ Kadai5-1/src/kadai/tomoyuri.java
@@ -11,14 +11,17 @@ public class tomoyuri {
 	}
 	
 		static String translateEng(int n) {
+			//int z=n/100;
 			int x=n/10;//10‚ÌˆÊ
 			int y=n-(x*10);//1‚ÌˆÊ
+			
 			if(n <= 10) return countEng1(n);
+			
 			if(n>=11 && 19>=n) return countEng3(n);
 			
 			if(n%10 == 0) return countEng2(n);
-			else if(n>20 && n<=101) return countEng2(n)+" "+countEng1(y);
-			else if(n>=111 && 119>=n) return countEng2(n)+" "+countEng3(y);
+			else if(n>20 && n<=109) return countEng2(n)+" "+countEng1(y);
+			else if(n>=111 && 119>=n) return countEng2(n)+" "+countEng3(n);
 			
 			return n+"";
 			
@@ -40,20 +43,21 @@ public class tomoyuri {
 		String[] b = {"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety","hundred"};
 		
 		
+		if(n>20 && n<=109) return b[n/10-2];
 		
-	    return b[n/10-2];
+		else return b[n/10-3];
+		}
 		
 			
-		}
+		
 		
 	
 	static String countEng3(int n){
-		int x=n/10;//10‚ÌˆÊ
-		int y=n-(x*10);//1‚ÌˆÊ
+		
 		String[] c = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
 		
 		
-		return c[y-1];
+		return c[n%10-1];
 	
 	
 		
